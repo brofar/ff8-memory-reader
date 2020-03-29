69e232a735b2f4ddde0c58f9db32a69b24a92882
Behfar Lotfizadeh <behfar@otafest.com>
2020-03-28 23:08:44 -0600

    Draw Points

---
+++ FF8 Memory Reader/FFVIIIDrawPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FF8_Memory_Reader
+{
+    class FFVIIIDrawPoint
+    {
+        public int locationId { get; set; }
+        public int spellId { get; set; }
+        public int memoryOffset { get; set; }
+    }
+}
